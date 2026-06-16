a355f1ba90985cc56a7330802252302223560ac9
Devil4324 <imonbloghoster9@gmail.com>
2026-06-16 21:32:26 +0530

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://rctjrobphjtxbouvjrus.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_HFgVv7e1bs-vudJb0LhLFQ_rUs4xGTz";
+
+const supabase =
+window.supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
